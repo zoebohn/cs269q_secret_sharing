f28fc1d9931a7704a0ebdf725470f74e20444f43
Zoë Bohn <zbohn2@Zoes-MacBook-Pro-3.local>
2019-05-18 14:23:26 -0700

    skeleton code

---
+++ secret_sharing.py
@@ -0,0 +1,18 @@
+from pyquil import Program
+from pyquil.gates import CNOT, CZ, H, X, Z
+
+def Alice(qubits, message):
+    pass
+
+def Bob(qubits):
+    pass
+
+def Charlie(qubits):
+    pass
+
+def initial_setup():
+    pass
+
+
+
+
