062bfe117b61f3ec80e14dc50f069c6896ee2032
Zoë Bohn <zbohn2@Zoes-MacBook-Pro-3.local>
2019-05-18 16:27:18 -0700

    I *think* I implemented the direction checking and the measurement stuff, now just need to have charlie and bob reconstruct what alice sent

--- secret_sharing.py
+++ secret_sharing.py
@@ -1,5 +1,5 @@
 from pyquil import Program
-from pyquil.gates import CNOT, CZ, H, X, Z
+from pyquil.gates import CNOT, H, S
 import random
 from pyquil.api import QVMConnection
 
@@ -10,23 +10,47 @@ def choose_random_direction():
     else:
         return 'y'
 
+# TODO - somewhat unsure of this
+    # measuring in x direction means applying an H gate
+    # measuring in the y direction means apply an H gate followed by an S_dagger gate
+    # (view in chrome): https://docs.microsoft.com/en-us/quantum/concepts/pauli-measurements?view=qsharp-preview
+def prepare_measurement(qubit, direction, program):
+    if direction == 'x':
+        program += H(qubit)
+    else:
+        program += H(qubit)
+        program_S_dagger = Program()
+        program_S_dagger += S(qubit)
+        program += program_S_dagger.dagger()
+
+    return program
+
+# TODO - how to incorporate message?
 def alice(qubit, message, program):
     alice_measure_dir = choose_random_direction()
-    # TODO
-    return alice_measure_dir
+    program = prepare_measurement(qubit, alice_measure_dir, program)
+    return alice_measure_dir, program
 
 def bob(qubit, program):
     bob_measure_dir = choose_random_direction()
-    # TODO
-    return bob_measure_dir
+    program = prepare_measurement(qubit, bob_measure_dir, program)
+    return bob_measure_dir, program
 
 def charlie(qubit, program):
     charlie_measure_dir = choose_random_direction()
-    # TODO 
-    return charlie_measure_dir
+    program = prepare_measurement(qubit, charlie_measure_dir, program)
+    return charlie_measure_dir, program
 
+# TODO - very unsure of this
 def check_directions(alice_measure_dir, bob_measure_dir, charlie_measure_dir):
-    pass
+    if alice_measure_dir == 'x' and bob_measure_dir == 'x':
+        return charlie_measure_dir == 'x'
+    if alice_measure_dir == 'x' and bob_measure_dir == 'y':
+        return charlie_measure_dir == 'y'
+    if alice_measure_dir == 'y' and bob_measure_dir == 'x':
+        return charlie_measure_dir == 'y'
+    if alice_measure_dir == 'y' and bob_measure_dir == 'y':
+        return charlie_measure_dir == 'x'
 
 def bob_and_charlie(bob_measure_result, charlie_measure_result):
     pass 
@@ -65,9 +89,9 @@ for trial in range(NUM_TRIALS):
     # perform secret sharing procedure once per message bit
     for i in range(MSG_LENGTH):
         while (True): # retry until success
-            alice_measure_dir = alice(alice_q[i], message, program)
-            bob_measure_dir = bob(bob_q[i], program)
-            charlie_measure_dir = charlie(charlie_q[i], program)
+            alice_measure_dir, program = alice(alice_q[i], message, program)
+            bob_measure_dir, program = bob(bob_q[i], program)
+            charlie_measure_dir, program = charlie(charlie_q[i], program)
             should_abort = check_directions(alice_measure_dir, bob_measure_dir, charlie_measure_dir)
             if should_abort:
                 print("Abort! Measurements yielded no useful information. Retrying...") 
@@ -75,7 +99,7 @@ for trial in range(NUM_TRIALS):
                 continue
             # run program, now that we know results will be interesting
             qvm = QVMConnection()
-            program = program.measure_all() # TODO - this is just measuring them all, not according to a particular direction
+            program = program.measure_all() 
             results = qvm.run(program)[0]
             alice_measure_result = results[0]
             bob_measure_result = results[1]
